d5bb508f8e5dac59947f2ea29139bdbfc5cb03f5
Ben Wanicur <bwanicur@gmail.com>
2018-08-30 12:48:27 -0700

    Adds airbnb and react linting

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends":  [
+    "airbnb-base",
+    "plugin:react/recommended"
+  ],
+  "rules": {
+    "semi": [2, "never"],
+    "import/no-unresolved": 0
+  }
+};
--- app/javascript/packs/gig_manager.jsx
+++ app/javascript/packs/gig_manager.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import { BrowserRouter } from 'react-router-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
@@ -11,7 +11,7 @@ import 'semantic-ui-css/semantic.css'
 
 // import rootReducer from '../src/reducers'
 // Temp while we work on the router
-const rootReducer = () => ( [] )
+const rootReducer = () => ([])
 const store = createStore(rootReducer)
 
 document.addEventListener('DOMContentLoaded', () => {
