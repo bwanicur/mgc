6c3ec28a25312887fcbf3952109e0f20d1e0cda7
Ben Wanicur <bwanicur@gmail.com>
2021-10-25 11:04:32 -0700

    Removes the react-router code
    Going to attempt a tabbed approach with a single "page" to keep it simple

--- app/javascript/src/gig_manager/components/app.jsx
+++ app/javascript/src/gig_manager/components/app.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-// import CollapseableNavigation from './navigation/collapseable_navigation'
-import Navigation from './navigation/navigation'
-import MainDisplay from './main/main_display'
+// TODO: use tabs instead of "pages" and routes
+// TODO: make the tabs "collapseable"
+// import Navigation from './navigation/navigation'
+// import MainDisplay from './main/main_display'
 
 import 'font-awesome/css/font-awesome.min.css';
 
 
-// TODO for smaller displays: <CollapseableNavigation />
 const App = () => (
   <div id="app-wrapper" className="ui container">
-    <Navigation />
-    <MainDisplay />
   </div>
 )
 
--- app/javascript/src/gig_manager/components/main/main_display.jsx
+++ app/javascript/src/gig_manager/components/main/main_display.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+// import { Switch, Route } from 'react-router-dom'
 import DashboardContainer from '../../containers/dashboard_container'
 import Musicians from '../musicians/musicians'
 import Gigs from '../gigs/gigs'
@@ -7,12 +7,12 @@ import Settings from '../settings/settings'
 
 const MainDisplay = () => (
   <div>
-    <Switch>
-      <Route exact path='/' component={DashboardContainer} />
-      <Route path='/musicians' component={Musicians} />
-      <Route path='/gigs' component={Gigs} />
-      <Route path='/settings' component={Settings} />
-    </Switch>
+    // <Switch>
+    //   <Route exact path='/' component={DashboardContainer} />
+    //   <Route path='/musicians' component={Musicians} />
+    //   <Route path='/gigs' component={Gigs} />
+    //   <Route path='/settings' component={Settings} />
+    // </Switch>
   </div>
 )
 
